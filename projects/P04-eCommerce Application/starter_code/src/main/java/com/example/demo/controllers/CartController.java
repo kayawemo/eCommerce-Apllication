d51a92b8479a1e5bd213f4423419846221b9c4a2
Kayode-Awe <kayode.awe@outlook.com>
2022-06-11 02:06:37 -0600

    Added codes for logging.

--- projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/controllers/CartController.java
+++ projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/controllers/CartController.java
@@ -3,6 +3,8 @@ package com.example.demo.controllers;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +24,9 @@ import com.example.demo.model.requests.ModifyCartRequest;
 @RestController
 @RequestMapping("/api/cart")
 public class CartController {
+
+	//For logging
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
 	
 	@Autowired
 	private UserRepository userRepository;
@@ -40,12 +45,16 @@ public class CartController {
 		}
 		Optional<Item> item = itemRepository.findById(request.getItemId());
 		if(!item.isPresent()) {
+			log.info("Item not present");
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
 		}
 		Cart cart = user.getCart();
 		IntStream.range(0, request.getQuantity())
 			.forEach(i -> cart.addItem(item.get()));
 		cartRepository.save(cart);
+		log.info("Item with ID " + request.getItemId() + " successfully added");
+
 		return ResponseEntity.ok(cart);
 	}
 	
